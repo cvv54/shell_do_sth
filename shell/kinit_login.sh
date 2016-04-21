88f3ce2674e41fafef41702d41fef40bdffc610b
cvv54 <291746769@qq.com>
2016-04-21 16:56:49 +0800

    update kinit_login.sh
    you should use you real REALM and hostname to replase it

--- shell/kinit_login.sh
+++ shell/kinit_login.sh
@@ -4,9 +4,9 @@
 
 	echo "---init Kerberos:---"
 	
-	kinit -kt /etc/security/keytabs/single.keytab single/gateway.bdsm.cmcc@BDSM.CMCC
+	kinit -kt /etc/security/keytabs/single.keytab single/host@REALM
 
 	echo "---login---"
 	
-	beeline -u "jdbc:hive2://gateway.bdsm.cmcc:10000/;principal=single/gateway.bdsm.cmcc@BDSM.CMCC"
+	beeline -u "jdbc:hive2://host:10000/;principal=single/host@REALM"
 
